3b2501eb9ead4058a6eb197bd7c0b07296d5c5b3
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 22:09:09 +0200

    Batch #140 commit - 1000 files

---
+++ contracts/mutants/FragmentToken/7/FVR/FragmentToken.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract FragmentToken is ERC20, Ownable {
+
+    using EnumerableSet for EnumerableSet.AddressSet;
+    EnumerableSet.AddressSet private _minters;
+
+    constructor(string memory name_, string memory symbol_) 
+    internal ERC20(name_, symbol_) {
+
+    }
+
+    function mint(address _to, uint256 _amount) external onlyMinter returns (bool) {
+
+        _mint(_to, _amount);
+
+        return true;
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+    
+    function addMinter(address _addMinter) external onlyOwner returns (bool) {
+        require(_addMinter != address(0), "FragmentToken: _addMinter is the zero address");
+        return EnumerableSet.add(_minters, _addMinter);
+    }
+
+    function delMinter(address _delMinter) external onlyOwner returns (bool) {
+        require(_delMinter != address(0), "FragmentToken: _delMinter is the zero address");
+        return EnumerableSet.remove(_minters, _delMinter);
+    }
+
+    function getMinterLength() external view returns (uint256) {
+        return EnumerableSet.length(_minters);
+    }
+
+    function isMinter(address account) external view returns (bool) {
+        return EnumerableSet.contains(_minters, account);
+    }
+
+    function getMinter(uint256 _index) public view onlyOwner returns (address) {
+        require(_index <= getMinterLength() - 1, "FragmentToken: index out of bounds");
+        return EnumerableSet.at(_minters, _index);
+    }
+
+    // modifier for mint function
+    modifier onlyMinter() {
+        require(isMinter(msg.sender), "FragmentToken: caller is not the minter");
+        _;
+    }
+}
