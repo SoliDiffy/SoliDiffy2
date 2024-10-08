6adcadf40a158f6591f747f329d7239be1182311
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 20:41:29 +0200

    Batch #118 commit - 1000 files

---
+++ contracts/mutants/EdenFormula/1/ORFD/EdenFormula.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../lib/VotingPowerFormula.sol";
+
+/**
+ * @title EdenFormula
+ * @dev Convert EDEN to voting power
+ */
+contract EdenFormula is VotingPowerFormula {
+    /**
+     * @notice Convert EDEN amount to voting power
+     * @dev Always converts 1-1
+     * @param amount token amount
+     * @return voting power amount
+     */
+    
+}
