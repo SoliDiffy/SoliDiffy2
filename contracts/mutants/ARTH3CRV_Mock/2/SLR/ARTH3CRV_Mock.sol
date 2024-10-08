36d0883dffaaae55b4ab2444a0c42f882096d5a4
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 16:30:22 +0200

    Batch #10 commit - 1000 files

---
+++ contracts/mutants/ARTH3CRV_Mock/2/SLR/ARTH3CRV_Mock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "";
+
+contract ARTH3CRV_Mock is ERC20 {
+    constructor() ERC20("", 'ARTH3CRV-f') {}
+}
---
+++ contracts/mutants/ARTH3CRV_V2_Mock/3/SLR/ARTH3CRV_V2_Mock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "";
+
+contract ARTH3CRV_V2_Mock is ERC20 {
+    constructor()
+        ERC20("", "")
+    {}
+}
