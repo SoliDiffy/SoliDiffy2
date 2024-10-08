083845166bb14d5895fa979987644aef0b53a1e9
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 17:28:01 +0200

    Batch #54 commit - 1000 files

---
+++ contracts/mutants/BrokenRemoteContractStub/1/CCD/BrokenRemoteContractStub.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.4.23;
+
+
+contract BrokenRemoteContractStub {
+  uint256 public testNumber;
+
+  
+
+  function add(uint256 _num1, uint256 _num2)
+    public
+    pure
+    returns (uint256)
+  {
+    // intentionally broken for testing
+    return _num1 + _num2 + 3;
+  }
+
+  function setTestNumber(uint256 _number)
+    public
+    returns (uint256)
+  {
+    testNumber = _number;
+    return testNumber;
+  }
+}
