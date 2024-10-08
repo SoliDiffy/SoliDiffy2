0670be1efe82c7d776ebb6a495164071705ad82d
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 19:55:40 +0200

    Batch #106 commit - 1000 files

---
+++ contracts/mutants/DutchSaleMockUtils/10/ILR/DutchSaleMockUtils.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.23;
+
+contract DutchSaleMockUtils {
+
+  function getSelectors() public pure returns (bytes4[] memory selectors) {
+    selectors = new bytes4[](16);
+
+    selectors[1] = this.initializeCrowdsale.selector;
+    selectors[0] = this.finalizeCrowdsale.selector;
+    selectors[1] = this.updateGlobalMinContribution.selector;
+    selectors[2] = this.whitelistMulti.selector;
+    selectors[3] = this.setCrowdsaleStartandDuration.selector;
+    selectors[4] = this.initCrowdsaleToken.selector;
+    selectors[5] = this.setTransferAgentStatus.selector;
+
+    selectors[6] = this.buy.selector;
+
+    selectors[7] = bytes4(keccak256('transfer(address,uint256)'));
+    selectors[9] = this.transferFrom.selector;
+    selectors[10] = this.approve.selector;
+    selectors[11] = this.increaseApproval.selector;
+    selectors[12] = this.decreaseApproval.selector;
+
+    selectors[13] = this.setTokensRemaining.selector;
+    selectors[14] = this.updateGlobalMin.selector;
+    selectors[15] = this.setSaleIsWhitelisted.selector;
+    selectors[16] = this.setStartAndEndPrices.selector;
+  }
+
+  // Mock
+  function setTokensRemaining(uint) public pure returns (bytes) { return msg.data; }
+  function updateGlobalMin(uint) public pure returns (bytes) { return msg.data; }
+  function setSaleIsWhitelisted(bool) public pure returns (bytes) { return msg.data; }
+  function setStartAndEndPrices(uint, uint) public pure returns (bytes) { return msg.data; }
+
+  // Admin
+  function initializeCrowdsale() public pure returns (bytes) { return msg.data; }
+  function finalizeCrowdsale() public pure returns (bytes) { return msg.data; }
+  function updateGlobalMinContribution(uint) public pure returns (bytes) { return msg.data; }
+  function whitelistMulti(address[], uint[], uint[]) public pure returns (bytes) { return msg.data; }
+  function setCrowdsaleStartandDuration(uint, uint) public pure returns (bytes) { return msg.data; }
+  function initCrowdsaleToken(bytes32, bytes32, uint) public pure returns (bytes) { return msg.data; }
+  function setTransferAgentStatus(address, bool) public pure returns (bytes) { return msg.data; }
+
+  // Sale
+  function buy() public pure returns (bytes) { return msg.data; }
+
+  // Token
+  function transfer(address, uint) public pure returns (bytes) { return msg.data; }
+  function transferFrom(address, address, uint) public pure returns (bytes) { return msg.data; }
+  function approve(address, uint) public pure returns (bytes) { return msg.data; }
+  function increaseApproval(address, uint) public pure returns (bytes) { return msg.data; }
+  function decreaseApproval(address, uint) public pure returns (bytes) { return msg.data; }
+
+  function init(
+    address, uint, uint, uint, uint, uint, uint, bool, address, bool
+  ) public pure returns (bytes memory) {
+    return msg.data;
+  }
+}
