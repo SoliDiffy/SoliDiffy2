827dff2878794d30a8b025c450f87f2a6157e04f
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 16:43:44 +0200

    Batch #25 commit - 1000 files

---
+++ contracts/mutants/ArrayUtils/5/DLR/ArrayUtils.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.23;
+
+library ArrayUtils {
+
+  function toBytes4Arr(bytes32[] storage _arr) internal pure returns (bytes4[] storage _conv) {
+    assembly { _conv := _arr }
+  }
+
+  function toAddressArr(bytes32[] storage _arr) internal pure returns (address[] storage _conv) {
+    assembly { _conv := _arr }
+  }
+
+  function toUintArr(bytes32[] storage _arr) internal pure returns (uint[] memory _conv) {
+    assembly { _conv := _arr }
+  }
+}
