10388ca39df0803a13c5bd14a29309d2d1cdd148
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 01:07:41 +0200

    Batch #177 commit - 1000 files

---
+++ contracts/mutants/IndividuallyCappedCrowdsaleImpl/1/CCD/IndividuallyCappedCrowdsaleImpl.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+import "../token/ERC20/IERC20.sol";
+import "../crowdsale/validation/IndividuallyCappedCrowdsale.sol";
+import "./CapperRoleMock.sol";
+
+contract IndividuallyCappedCrowdsaleImpl
+  is IndividuallyCappedCrowdsale, CapperRoleMock {
+
+  
+}
---
+++ contracts/mutants/InitializableImmutableAdminUpgradeabilityProxy/3/SLR/InitializableImmutableAdminUpgradeabilityProxy.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.7;
+
+import {InitializableUpgradeabilityProxy} from "";
+import {Proxy} from "";
+import {BaseImmutableAdminUpgradeabilityProxy} from "";
+
+/**
+ * @title InitializableAdminUpgradeabilityProxy
+ * @author Aave
+ * @dev Extends BaseAdminUpgradeabilityProxy with an initializer function
+ */
+contract InitializableImmutableAdminUpgradeabilityProxy is
+  BaseImmutableAdminUpgradeabilityProxy,
+  InitializableUpgradeabilityProxy
+{
+  constructor(address admin) BaseImmutableAdminUpgradeabilityProxy(admin) {}
+
+  /// @inheritdoc BaseImmutableAdminUpgradeabilityProxy
+  function _willFallback() internal override(BaseImmutableAdminUpgradeabilityProxy, Proxy) {
+    BaseImmutableAdminUpgradeabilityProxy._willFallback();
+  }
+}
