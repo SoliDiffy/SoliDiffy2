384f34c3bca5d17fd9b448d596987ba82866e87a
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 20:45:31 +0200

    Batch #119 commit - 1000 files

---
+++ contracts/mutants/Encoder/6/OLFD/Encoder.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.8;
+
+library Encoder {
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+}
---
+++ contracts/mutants/Entry/6/TOR/Entry.sol
@@ -0,0 +1,74 @@
+pragma solidity >=0.4.21 <0.6.0;
+import "../utils/TokenClaimer.sol";
+import "../utils/Ownable.sol";
+import "../utils/SafeMath.sol";
+import "../erc20/IERC20.sol";
+import "../erc20/ERC20Impl.sol";
+import "../utils/Address.sol";
+import "../erc20/SafeERC20.sol";
+import "./Interfaces.sol";
+
+
+
+
+contract TradeInterface{
+   function get_total_chip() public view returns(uint256);
+
+}
+
+contract Entry is Ownable{
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    address public stable_token;
+    address public chip;
+    TradeInterface public vtrade;
+    uint256 public min_amount;
+    constructor(address _stable_token, address _chip, address _vtrade) public{
+        stable_token = _stable_token;
+        chip = _chip;
+        vtrade = TradeInterface(_vtrade);
+    }
+
+    event PUSDDeposit(uint256 stable_amount, uint256 chip_amount);
+    function deposit(uint256 _amount) public{
+        require(_amount >= min_amount, "too small amount");
+        uint before = IERC20(stable_token).balanceOf(address(this));
+        IERC20(stable_token).safeTransferFrom(tx.origin, address(this), _amount);
+        uint _after = IERC20(stable_token).balanceOf(address(this));
+        uint amount = _after.safeSub(before);
+        uint chip_amount;
+        if (before == 0){
+            chip_amount = amount;
+        }
+        else{
+            chip_amount = vtrade.get_total_chip().safeMul(amount).safeDiv(before);
+        }
+        TokenInterface(chip).generateTokens(tx.origin, chip_amount);
+        emit PUSDDeposit(amount, chip_amount);
+    }
+    event PUSDWithdraw(uint256 stable_amount, uint256 chip_amount);
+    function withdraw(uint256 _amount) public{
+        require(IERC20(chip).balanceOf(tx.origin) >= _amount, "not enough chip");
+        uint256 total_chip = TradeInterface(vtrade).get_total_chip();
+        uint256 stable_amount = _amount.safeMul(IERC20(stable_token).balanceOf(address(this))).safeDiv(total_chip);
+        require(stable_amount <= IERC20(stable_token).balanceOf(address(this)), "error: not enough pusd supply");
+        TokenInterface(chip).destroyTokens(tx.origin, _amount);
+        IERC20(stable_token).safeTransfer(tx.origin, stable_amount);
+        emit PUSDWithdraw(stable_amount, _amount);
+    }
+    event ChangeMinPUSDAmount(uint256 new_amount);
+    function change_min_amount(uint256 _amount) public onlyOwner{
+        min_amount = _amount;
+        emit ChangeMinPUSDAmount(_amount);
+    }
+}
+
+contract EntryFactory{
+  event CreateEntry(address addr);
+  function newEntry(address _stable_token, address _chip, address _vtrade) public returns(address){
+    Entry vt = new Entry(_stable_token, _chip, _vtrade);
+    emit CreateEntry(address(vt));
+    vt.transferOwnership(tx.origin);
+    return address(vt);
+  }
+}
