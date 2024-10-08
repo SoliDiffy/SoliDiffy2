7a011cb8565aa167e2048362e61a191b5f4d1990
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 18:28:03 +0200

    Batch #80 commit - 1000 files

---
+++ contracts/mutants/CreamERC20Market/8/ORFD/CreamERC20Market.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.3;
+
+import {SafeERC20} from "../../libs/SafeERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {
+    AddressUpgradeable
+} from "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import {MoneyMarket} from "../MoneyMarket.sol";
+import {DecMath} from "../../libs/DecMath.sol";
+import {ICrERC20} from "./imports/ICrERC20.sol";
+
+contract CreamERC20Market is MoneyMarket {
+    using DecMath for uint256;
+    using SafeERC20 for ERC20;
+    using AddressUpgradeable for address;
+
+    uint256 internal constant ERRCODE_OK = 0;
+
+    ICrERC20 public cToken;
+    ERC20 public override stablecoin;
+
+    function initialize(
+        address _cToken,
+        address _rescuer,
+        address _stablecoin
+    ) external initializer {
+        __MoneyMarket_init(_rescuer);
+        // Verify input addresses
+        require(
+            _cToken.isContract() && _stablecoin.isContract(),
+            "CreamERC20Market: An input address is not a contract"
+        );
+
+        cToken = ICrERC20(_cToken);
+        stablecoin = ERC20(_stablecoin);
+    }
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    /**
+        @dev See {Rescuable._authorizeRescue}
+     */
+    
+
+    uint256[48] private __gap;
+}
