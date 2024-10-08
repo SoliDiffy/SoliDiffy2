9463554dc906e9792f411d5bd5ebe008d0c13b10
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 23:12:06 +0200

    Batch #154 commit - 1000 files

---
+++ contracts/mutants/GeneralTransferManagerProxy/1/CCD/GeneralTransferManagerProxy.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.8;
+
+import "../../../proxy/OwnedUpgradeabilityProxy.sol";
+import "./GeneralTransferManagerStorage.sol";
+import "../../../Pausable.sol";
+import "../../../storage/modules/ModuleStorage.sol";
+
+/**
+ * @title Transfer Manager module for core transfer validation functionality
+ */
+contract GeneralTransferManagerProxy is GeneralTransferManagerStorage, ModuleStorage, Pausable, OwnedUpgradeabilityProxy {
+    /**
+    * @notice Constructor
+    * @param _securityToken Address of the security token
+    * @param _polyAddress Address of the polytoken
+    * @param _implementation representing the address of the new implementation to be set
+    */
+    
+
+}
