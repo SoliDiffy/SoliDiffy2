219486aeb5314d3a35473fd51b0c46db39ea0921
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 21:24:06 +0200

    Batch #129 commit - 1000 files

---
+++ contracts/mutants/ExpiringMultiParty/1/CCD/ExpiringMultiParty.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./Liquidatable.sol";
+
+/**
+ * @title Expiring Multi Party.
+ * @notice Convenient wrapper for Liquidatable.
+ */
+contract ExpiringMultiParty is Liquidatable {
+    /**
+     * @notice Constructs the ExpiringMultiParty contract.
+     * @param params struct to define input parameters for construction of Liquidatable. Some params
+     * are fed directly into the PricelessPositionManager's constructor within the inheritance tree.
+     */
+    
+}
