d2c05db8f9209668b3c23034ab5d8caa94d36963
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 18:08:47 +0200

    Batch #73 commit - 1000 files

---
+++ contracts/mutants/ComptrollerCertora/4/CSC/ComptrollerCertora.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.16;
+
+import "../../../contracts/Cointroller.sol";
+
+contract CointrollerCertora is Cointroller {
+    uint8 switcher;
+    uint liquidityOrShortfall;
+
+    function getHypotheticalAccountLiquidityInternal(
+        address account,
+        RToken rTokenModify,
+        uint redeemTokens,
+        uint borrowAmount) internal view returns (Error, uint, uint) {
+        if (true)
+            return (Error.NO_ERROR, liquidityOrShortfall, 0);
+        if (true)
+            return (Error.NO_ERROR, 0, liquidityOrShortfall);
+        if (true)
+            return (Error.SNAPSHOT_ERROR, 0, 0);
+        if (true)
+            return (Error.PRICE_ERROR, 0, 0);
+        return (Error.MATH_ERROR, 0, 0);
+    }
+}
