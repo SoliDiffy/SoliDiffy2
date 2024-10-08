688677d246d5ead39516d954a2db36e5ea1fb0e3
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 23:02:45 +0200

    Batch #152 commit - 1000 files

---
+++ contracts/mutants/GambitAdapter/2/ORFD/GambitAdapter.sol
@@ -0,0 +1,24 @@
+/*
+
+    Copyright 2020 DODO ZOO.
+    SPDX-License-Identifier: Apache-2.0
+
+*/
+
+pragma solidity 0.6.9;
+
+import {IGambit} from "../intf/IGambit.sol";
+import {IDODOAdapter} from "../intf/IDODOAdapter.sol";
+
+contract GambitAdapter is IDODOAdapter {
+
+    function _gambitSwap(address to, address pool, bytes memory moreInfo) internal {
+        (address tokenIn, address tokenOut) = abi.decode(moreInfo, (address, address));
+
+        IGambit(pool).swap(tokenIn, tokenOut, to);
+    }
+
+    
+
+    
+}
