f0978c624583be0333200ca5e4e91e39665b6296
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 16:47:00 +0200

    Batch #28 commit - 1000 files

---
+++ contracts/mutants/Audius/6/ORFD/Audius.sol
@@ -0,0 +1,101 @@
+// SPDX-FileCopyrightText: 2021 Tenderize <info@tenderize.me>
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../../libs/MathUtils.sol";
+
+import "../../Tenderizer.sol";
+import "../../WithdrawalPools.sol";
+import "./IAudius.sol";
+
+import { ITenderSwapFactory } from "../../../tenderswap/TenderSwapFactory.sol";
+
+contract Audius is Tenderizer {
+    using WithdrawalPools for WithdrawalPools.Pool;
+
+    // Eventws for WithdrawalPool
+    event ProcessUnstakes(address indexed from, address indexed node, uint256 amount);
+    event ProcessWithdraws(address indexed from, uint256 amount);
+
+    IAudius audius;
+
+    address audiusStaking;
+
+    WithdrawalPools.Pool withdrawPool;
+
+    function initialize(
+        IERC20 _steak,
+        string calldata _symbol,
+        IAudius _audius,
+        address _node,
+        uint256 _protocolFee,
+        uint256 _liquidityFee,
+        ITenderToken _tenderTokenTarget,
+        TenderFarmFactory _tenderFarmFactory,
+        ITenderSwapFactory _tenderSwapFactory
+    ) public {
+        Tenderizer._initialize(
+            _steak,
+            _symbol,
+            _node,
+            _protocolFee,
+            _liquidityFee,
+            _tenderTokenTarget,
+            _tenderFarmFactory,
+            _tenderSwapFactory
+        );
+        audius = _audius;
+        audiusStaking = audius.getStakingAddress();
+    }
+
+    
+
+    
+
+    
+
+    function processUnstake(address _node) external onlyGov {
+        uint256 amount = withdrawPool.processUnlocks();
+
+        // if no _node is specified, use default
+        address node_ = _node;
+        if (node_ == address(0)) {
+            node_ = node;
+        }
+        
+        // Undelegate from audius
+        audius.requestUndelegateStake(node_, amount);
+
+        emit ProcessUnstakes(msg.sender, node_, amount);
+    }
+
+    
+
+    function processWithdraw(address /* _node */) external onlyGov {
+        uint256 balBefore = steak.balanceOf(address(this));
+        
+        audius.undelegateStake();
+        
+        uint256 balAfter = steak.balanceOf(address(this));
+        uint256 amount = balAfter - balBefore;
+        
+        withdrawPool.processWihdrawal(amount);
+
+        emit ProcessWithdraws(msg.sender, amount);
+    }
+
+    
+
+
+    
+
+    function _setStakingContract(address _stakingContract) internal override {
+        audius = IAudius(_stakingContract);
+        audiusStaking = audius.getStakingAddress();
+
+        emit GovernanceUpdate("STAKING_CONTRACT");
+    }
+}
---
+++ contracts/mutants/AuthManager/3/ORFD/AuthManager.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+import "../interfaces/IAuthManager.sol";
+
+
+contract AuthManager is IAuthManager, Initializable {
+    mapping(address => bytes32[])  internal members;
+    uint256 internal constant NOTFOUND = type(uint256).max;
+    bytes32 public constant SUPER_ROLE = keccak256("SUPER_ROLE");
+
+    event AddMember(address member, bytes32 role);
+    event RemoveMember(address member, bytes32 role);
+
+    function initialize(address superior) external initializer {
+        if (superior == address(0)) {
+            members[msg.sender] = [SUPER_ROLE];
+            emit AddMember(msg.sender, SUPER_ROLE);
+        } else {
+            members[superior] = [SUPER_ROLE];
+            emit AddMember(msg.sender, SUPER_ROLE);
+        }
+    }
+
+    function roles(address _member) external view returns (bytes32[] memory) {
+        return members[_member];
+    }
+
+    
+
+    
+
+    function addByString(string calldata roleString, address member) external {
+        require(_find(members[msg.sender], SUPER_ROLE) != NOTFOUND, "FORBIDDEN");
+
+        bytes32[] storage _roles = members[member];
+        bytes32 role = keccak256(bytes(roleString));
+
+        require(_find(_roles, role) == NOTFOUND, "ALREADY_MEMBER");
+        _roles.push(role);
+        emit AddMember(member, role);
+    }
+
+    
+
+    function _find(bytes32[] storage _roles, bytes32 _role) internal view returns (uint256) {
+        for (uint256 i = 0; i < _roles.length; ++i) {
+            if (_role == _roles[i]) {
+                return i;
+            }
+        }
+        return NOTFOUND;
+    }
+
+}
