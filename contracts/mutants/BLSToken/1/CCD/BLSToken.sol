dc97dad11aacf100aedeb2f7322a614feaf8839c
SoliDiffy <limonyassistant@gmail.com>
2024-10-08 16:51:39 +0200

    Batch #32 commit - 1000 files

---
+++ contracts/mutants/BLSToken/1/CCD/BLSToken.sol
@@ -0,0 +1,17 @@
+// SWC-102-Outdated Compiler Version: L2
+pragma solidity 0.8.5;
+//SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BLSToken is ERC20 {
+
+    uint maxSupply = 42000000 ether; // 42 million max tokens
+
+    
+
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+}
