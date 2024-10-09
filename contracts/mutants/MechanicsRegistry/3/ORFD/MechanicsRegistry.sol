20b519fe6d270c16f7350d6d29e827ca8f56d664
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 04:50:08 +0200

    Batch #214 commit - 1000 files

---
+++ contracts/mutants/MechanicsRegistry/3/ORFD/MechanicsRegistry.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import '../interfaces/mechanics/IMechanicsRegistry.sol';
+import '../abstract/UtilsReady.sol';
+
+contract MechanicsRegistry is UtilsReady, IMechanicsRegistry {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  EnumerableSet.AddressSet internal _mechanics;
+
+  constructor(address _mechanic) {
+    _addMechanic(_mechanic);
+  }
+
+  // Setters
+  
+
+  
+
+  function _addMechanic(address _mechanic) internal {
+    require(_mechanic != address(0), 'MechanicsRegistry::add-mechanic:mechanic-should-not-be-zero-address');
+    require(!_mechanics.contains(_mechanic), 'MechanicsRegistry::add-mechanic:mechanic-already-added');
+    _mechanics.add(_mechanic);
+    emit MechanicAdded(_mechanic);
+  }
+
+  function _removeMechanic(address _mechanic) internal {
+    require(_mechanics.contains(_mechanic), 'MechanicsRegistry::remove-mechanic:mechanic-not-found');
+    _mechanics.remove(_mechanic);
+    emit MechanicRemoved(_mechanic);
+  }
+
+  // View helpers
+  
+
+  // Getters
+  function mechanics() public view override returns (address[] memory _mechanicsList) {
+    _mechanicsList = new address[](_mechanics.length());
+    for (uint256 i; i < _mechanics.length(); i++) {
+      _mechanicsList[i] = _mechanics.at(i);
+    }
+  }
+}
---
+++ contracts/mutants/MediatorOwnableModule/1/CCD/MediatorOwnableModule.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "./OwnableModule.sol";
+
+/**
+ * @title MediatorOwnableModule
+ * @dev Common functionality for non-upgradeable Omnibridge extension module.
+ */
+contract MediatorOwnableModule is OwnableModule {
+    address public mediator;
+
+    /**
+     * @dev Initializes this contract.
+     * @param _mediator address of the deployed Omnibridge extension for which this module is deployed.
+     * @param _owner address of the owner that is allowed to perform additional actions on the particular module.
+     */
+    
+
+    /**
+     * @dev Throws if sender is not the Omnibridge extension.
+     */
+    modifier onlyMediator {
+        require(msg.sender == mediator);
+        _;
+    }
+}
