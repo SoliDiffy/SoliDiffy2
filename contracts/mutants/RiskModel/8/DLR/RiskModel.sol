5b9f911c822f82de402df4ae10bf216fab1d8e69
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 15:44:57 +0200

    Batch #3 commit - 1000 files

---
+++ contracts/mutants/RiskModel/8/DLR/RiskModel.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Risk {
+    struct Data {
+        mapping(string => RiskItem) riskItems;
+    }
+
+    struct RiskItem {
+        uint256 interestRate;
+        uint256 advanceRate;
+    }
+    
+    function set(Data memory self, string storage key, uint256 interestRate, uint256 advanceRate) public {
+        self.riskItems[key] = RiskItem(interestRate, advanceRate);
+    }
+
+    function getInterestRate(Data memory self, string storage key) public view returns (uint256) {
+        return self.riskItems[key].interestRate;
+    }
+
+    function getAdvanceRate(Data memory self, string storage key) public view returns (uint256) {
+        return self.riskItems[key].advanceRate;
+    }
+
+    function remove(Data memory self, string storage key) public {
+        delete self.riskItems[key];
+    }
+}
---
+++ contracts/mutants/RobotLiability/1/CCD/RobotLiability.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+import './RobotLiabilityAPI.sol';
+import './LightContract.sol';
+
+// Standard robot liability light contract
+contract RobotLiability is RobotLiabilityAPI, LightContract {
+    
+}
