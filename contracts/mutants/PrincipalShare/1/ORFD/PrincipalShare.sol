85a46d7a1613b52474664c1df95cd205120ac81d
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 12:59:00 +0200

    Batch #16 commit - 1000 files

---
+++ contracts/mutants/PrincipalShare/1/ORFD/PrincipalShare.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.6;
+
+import "./PoolShare.sol";
+
+/// @dev Token representing the principal shares of a pool.
+contract PrincipalShare is PoolShare {
+    // solhint-disable-next-line no-empty-blocks
+    constructor(
+        ITempusPool _pool,
+        string memory name,
+        string memory symbol
+    ) PoolShare(ShareKind.Principal, _pool, name, symbol) {}
+
+    
+
+    function getPricePerFullShareStored() external view override returns (uint256) {
+        return pool.pricePerPrincipalShareStored();
+    }
+}
---
+++ contracts/mutants/PrincipalShare/2/ORFD/PrincipalShare.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.6;
+
+import "./PoolShare.sol";
+
+/// @dev Token representing the principal shares of a pool.
+contract PrincipalShare is PoolShare {
+    // solhint-disable-next-line no-empty-blocks
+    constructor(
+        ITempusPool _pool,
+        string memory name,
+        string memory symbol
+    ) PoolShare(ShareKind.Principal, _pool, name, symbol) {}
+
+    
+
+    
+}
