b9d692d5ac482e0f8b9e71284fd36b2f88b30b03
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 09:53:39 +0200

    Batch #255 commit - 1000 files

---
+++ contracts/mutants/PNaivePriceOracle/2/FVR/PNaivePriceOracle.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.21 <0.6.0;
+import "../utils/Ownable.sol";
+
+contract PNaivePriceOracle is Ownable{
+  //price of per 10**decimals()
+  uint256 public price;
+  function setPrice(uint256 _price) external onlyOwner{
+    price = _price;
+  }
+
+  function getPrice() external view returns(uint256){
+    return price;
+  }
+}
