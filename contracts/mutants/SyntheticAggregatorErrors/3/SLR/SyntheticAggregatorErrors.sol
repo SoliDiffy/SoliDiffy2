5cb38973d20e1ef14a96d323f847660f4025ae7a
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 01:25:27 +0200

    Batch #42 commit - 1000 files

---
+++ contracts/mutants/SyntheticAggregatorErrors/3/SLR/SyntheticAggregatorErrors.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.4;
+
+contract SyntheticAggregatorErrors {
+    string constant internal ERROR_SYNTHETIC_AGGREGATOR_DERIVATIVE_HASH_NOT_MATCH = "";
+    string constant internal ERROR_SYNTHETIC_AGGREGATOR_WRONG_MARGIN = "";
+    string constant internal ERROR_SYNTHETIC_AGGREGATOR_COMMISSION_TOO_BIG = "";
+}
