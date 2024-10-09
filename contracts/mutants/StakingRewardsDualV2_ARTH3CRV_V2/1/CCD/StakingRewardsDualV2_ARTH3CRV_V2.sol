d508218666398a99720baf4fd75ff3822af05f8e
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 22:49:49 +0200

    Batch #24 commit - 1000 files

---
+++ contracts/mutants/StakingRewardsDualV2_ARTH3CRV_V2/1/CCD/StakingRewardsDualV2_ARTH3CRV_V2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import '../StakingRewardsDualV2.sol';
+
+contract StakingRewardsDualV2_ARTH3CRV_V2 is StakingRewardsDualV2 {
+    
+}
---
+++ contracts/mutants/StakingRewardsDualV2_FRAX3CRV_V2/1/CCD/StakingRewardsDualV2_FRAX3CRV_V2.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "../StakingRewardsDualV2.sol";
+
+contract StakingRewardsDualV2_FRAX3CRV_V2 is StakingRewardsDualV2 {
+    
+}
