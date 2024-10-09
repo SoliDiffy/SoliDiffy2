deb370a7e0179ec1145b8fa3f08a50451f7c8181
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 08:02:41 +0200

    Batch #241 commit - 1000 files

---
+++ contracts/mutants/NodeOperatorsRegistryMockForSecurityModule/2/FVR/NodeOperatorsRegistryMockForSecurityModule.sol
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2021 Lido <info@lido.fi>
+
+// SPDX-License-Identifier: GPL-3.0
+
+/* See contracts/COMPILERS.md */
+pragma solidity 0.8.9;
+
+contract NodeOperatorsRegistryMockForSecurityModule {
+    uint256 internal keysOpIndex;
+
+    function getKeysOpIndex() public view returns (uint256 index) {
+        return keysOpIndex;
+    }
+
+    function setKeysOpIndex(uint256 _newKeysOpIndex) public {
+        keysOpIndex = _newKeysOpIndex;
+    }
+}
