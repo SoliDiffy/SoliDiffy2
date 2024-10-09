791a3b3bbd527b38a52c2323bb12bae03598c98b
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 08:25:58 +0200

    Batch #244 commit - 1000 files

---
+++ contracts/mutants/OKLGProduct/6/FVR/OKLGProduct.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/interfaces/IERC20.sol';
+import './interfaces/IOKLGSpend.sol';
+import './OKLGWithdrawable.sol';
+
+/**
+ * @title OKLGProduct
+ * @dev Contract that every product developed in the OKLG ecosystem should implement
+ */
+contract OKLGProduct is OKLGWithdrawable {
+  IERC20 private _token; // OKLG
+  IOKLGSpend private _spend;
+
+  uint8 public productID;
+
+  constructor(
+    uint8 _productID,
+    address _tokenAddy,
+    address _spendAddy
+  ) {
+    productID = _productID;
+    _token = IERC20(_tokenAddy);
+    _spend = IOKLGSpend(_spendAddy);
+  }
+
+  function setTokenAddy(address _tokenAddy) public onlyOwner {
+    _token = IERC20(_tokenAddy);
+  }
+
+  function setSpendAddy(address _spendAddy) public onlyOwner {
+    _spend = IOKLGSpend(_spendAddy);
+  }
+
+  function setProductID(uint8 _newId) public onlyOwner {
+    productID = _newId;
+  }
+
+  function getTokenAddress() external view returns (address) {
+    return address(_token);
+  }
+
+  function getSpendAddress() external view returns (address) {
+    return address(_spend);
+  }
+
+  function _payForService(uint256 _weiToRemoveFromSpend) public {
+    _spend.spendOnProduct{ value: msg.value - _weiToRemoveFromSpend }(
+      msg.sender,
+      productID
+    );
+  }
+}
