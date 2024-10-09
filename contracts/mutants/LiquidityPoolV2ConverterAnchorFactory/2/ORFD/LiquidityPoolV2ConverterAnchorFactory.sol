3fe7dca09d968156fb0ff31a7b482b904d94e4f8
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 03:26:14 +0200

    Batch #201 commit - 1000 files

---
+++ contracts/mutants/LiquidityPoolV2ConverterAnchorFactory/2/ORFD/LiquidityPoolV2ConverterAnchorFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.6.12;
+import "./PoolTokensContainer.sol";
+import "../../interfaces/ITypedConverterAnchorFactory.sol";
+
+/*
+    LiquidityPoolV2ConverterAnchorFactory Factory
+*/
+contract LiquidityPoolV2ConverterAnchorFactory is ITypedConverterAnchorFactory {
+    /**
+      * @dev returns the converter type the factory is associated with
+      *
+      * @return converter type
+    */
+    
+
+    /**
+      * @dev creates a new converter anchor with the given arguments and transfers
+      * the ownership to the caller
+      *
+      * @param _name        pool name
+      * @param _symbol      pool symbol
+      * @param _decimals    pool decimals
+      *
+      * @return new anchor
+    */
+    
+}
