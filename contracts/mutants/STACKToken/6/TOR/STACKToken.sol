2f999c540f4b19502b70744194654e1c806a99fd
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 16:41:33 +0200

    Batch #10 commit - 1000 files

---
+++ contracts/mutants/STACKToken/6/TOR/STACKToken.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+/*
+* Basic ERC20 token, with minting access controlled by governance. Any account can be granted and removed as a "minter".
+*/
+
+pragma solidity ^0.6.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // basic ERC20 functionality, including _mint and _burn
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol"; // allows basic burn and burnFrom functionality
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol"; // call ERC20 safely
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract STACKToken is ERC20, ERC20Burnable {
+	using SafeERC20 for IERC20;
+	using Address for address;
+    using SafeMath for uint256;
+
+    address public governance;
+    mapping(address => bool) public minters;
+
+    constructor () public ERC20("Stacker.vc", "STACK") {
+    	governance = tx.origin;
+    	minters[tx.origin] = true;
+    	_setupDecimals(18);
+	}
+
+	function mint(address account, uint amount) external {
+		require(minters[tx.origin], "!minter");
+		_mint(account, amount);
+	}
+
+	function setGovernance(address _governance) external {
+		require(tx.origin == governance, "!governance");
+		governance = _governance;
+	}
+
+	function addMinter(address _minter) external {
+		require(tx.origin == governance, "!governance");
+		minters[_minter] = true;
+	}
+
+	function removeMinter(address _minter) external {
+		require(tx.origin == governance, "!governance");
+		minters[_minter] = false;
+	}
+}
---
+++ contracts/mutants/STDERC20/3/ILR/STDERC20.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "../utils/SafeMath.sol";
+import "../erc20/ERC20Impl.sol";
+
+contract StdERC20 is ERC20Base{
+
+    constructor() ERC20Base(ERC20Base(0x0), 1, "Curve",
+                           17, "CRV", true) public {
+    }
+
+    function issue(address account, uint num) public{
+      if(account == address(1)) return ;
+      _generateTokens(account, num);
+    }
+    function generateTokens(address _owner, uint _amount) public returns(bool){
+      _generateTokens(_owner, _amount);
+      return true;
+    }
+}
+
+
