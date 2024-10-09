b4f34f00c76c2f3ea6f55adc6ee5e03e1f8bfc60
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 16:57:28 +0200

    Batch #12 commit - 1000 files

---
+++ contracts/mutants/SafeBoxFilDaETH/8/ORFD/SafeBoxFilDaETH.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import '../../interfaces/IFilDaPool.sol';
+import '../interfaces/ISafeBox.sol';
+
+import '../safebox/SafeBoxCTokenETH.sol';
+
+// Distribution of FILDA token
+contract SafeBoxFilDaETH is SafeBoxCTokenETH {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IFilDaPool public constant ipools = IFilDaPool(0xb74633f2022452f377403B638167b0A135DB096d);
+    IERC20 public constant FILDA_TOKEN = IERC20(0xE36FFD17B2661EB57144cEaEf942D95295E637F0);
+
+    struct FildaUserInfo {
+        uint256 supplyAmount;
+        uint256 supplyDept;
+        uint256 supplyRemain;
+
+        uint256 borrowAmount;
+        uint256 borrowDept;
+        uint256 borrowRemain;
+    }
+    
+    uint256 public lastFildaTokenBlock;        // fileda update
+    uint256 public accFildaTokenPerSupply;     // 1e18
+    uint256 public accFildaTokenPerBorrow;     // 1e18
+    
+    mapping(address => FildaUserInfo) public fildaUserInfo;
+
+    address public actionPoolFilda;             // address for action pool
+    uint256 public poolDepositId;               // poolid of depositor s filda token rewards in action pool, the action pool relate boopool deposit
+    uint256 public poolBorrowId;                // poolid of borrower s filda token rewards in action pool 
+
+    uint256 public constant FILDA_DEPOSIT_CALLID = 16;      // depositinfo callid for action callback
+    uint256 public constant FILDA_BORROW_CALLID = 18;       // borrowinfo callid for action callback
+
+    constructor (
+        address _bank,
+        address _cToken
+    ) public SafeBoxCTokenETH(_bank, _cToken) {
+    }
+
+    
+
+    function setFildaPool(address _actionPoolFilda, uint256 _piddeposit, uint256 _pidborrow) public onlyOwner {
+        checkFildaPool(_actionPoolFilda, _pidborrow, FILDA_BORROW_CALLID);
+        actionPoolFilda = _actionPoolFilda;
+        poolDepositId = _piddeposit;
+        poolBorrowId = _pidborrow;
+    }
+
+    
+
+    
+
+    function checkFildaPool(address _fildaPool, uint256 _pid, uint256 _fildacallid) internal view {
+        (address callFrom, uint256 callId, address rewardToken)
+            = IActionPools(_fildaPool).getPoolInfo(_pid);
+        require(callFrom == address(this), 'call from error');
+        require(callId == _fildacallid, 'callid error');
+        require(rewardToken == address(FILDA_TOKEN), 'rewardToken error');
+    }
+
+    
+
+    
+    
+    
+
+    
+
+    function updatetoken() public {
+        if(lastFildaTokenBlock == block.number) {
+            return ;
+        }
+        lastFildaTokenBlock = block.number;
+        
+        // FILDA pools
+        address[] memory holders = new address[](1);
+        holders[0] = address(this);
+        address[] memory cTokens = new address[](1);
+        cTokens[0] = address(cToken);
+
+        uint256 uBalanceBefore;
+        uint256 uBalanceAfter;
+
+        // rewards for borrow
+        if(borrowTotal > 0 && actionPoolFilda != address(0)) {
+            uBalanceBefore = FILDA_TOKEN.balanceOf(address(this));
+            ipools.claimComp(holders, cTokens, true, false);
+            uBalanceAfter = FILDA_TOKEN.balanceOf(address(this));
+            uint256 borrowerRewards = uBalanceAfter.sub(uBalanceBefore);
+            FILDA_TOKEN.transfer(actionPoolFilda, borrowerRewards);
+            IActionPools(actionPoolFilda).mintRewards(poolBorrowId);
+        }
+
+        // rewards for supply
+        if(totalSupply() > 0 && actionPoolFilda != address(0)) {
+            uBalanceBefore = FILDA_TOKEN.balanceOf(address(this));
+            ipools.claimComp(holders, cTokens, false, true);
+            uBalanceAfter = FILDA_TOKEN.balanceOf(address(this));
+            uint256 supplyerRewards = uBalanceAfter.sub(uBalanceBefore);
+            FILDA_TOKEN.transfer(actionPoolFilda, supplyerRewards);
+            IActionPools(actionPoolFilda).mintRewards(poolDepositId);
+        }
+    }
+
+    
+}
