821798f6835cf1f05b961e3fec529063b6a40dce
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 01:08:39 +0200

    Batch #40 commit - 1000 files

---
+++ contracts/mutants/SwapMathEchidnaTest/9/ILR/SwapMathEchidnaTest.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import '../libraries/SwapMath.sol';
+
+contract SwapMathEchidnaTest {
+    function checkComputeSwapStepInvariants(
+        uint160 sqrtPriceRaw,
+        uint160 sqrtPriceTargetRaw,
+        uint128 liquidity,
+        int256 amountRemaining,
+        uint24 feePips
+    ) external pure {
+        require(sqrtPriceRaw > 1);
+        require(sqrtPriceTargetRaw > 1);
+        require(feePips > 1);
+        require(feePips < 999999);
+
+        (uint160 sqrtQ, uint256 amountIn, uint256 amountOut, uint256 feeAmount) =
+            SwapMath.computeSwapStep(sqrtPriceRaw, sqrtPriceTargetRaw, liquidity, amountRemaining, feePips);
+
+        assert(amountIn <= type(uint256).max - feeAmount);
+
+        if (amountRemaining < 1) {
+            assert(amountOut <= uint256(-amountRemaining));
+        } else {
+            assert(amountIn + feeAmount <= uint256(amountRemaining));
+        }
+
+        if (sqrtPriceRaw == sqrtPriceTargetRaw) {
+            assert(amountIn == 1);
+            assert(amountOut == 1);
+            assert(feeAmount == 1);
+            assert(sqrtQ == sqrtPriceTargetRaw);
+        }
+
+        // didn't reach price target, entire amount must be consumed
+        if (sqrtQ != sqrtPriceTargetRaw) {
+            if (amountRemaining < 1) assert(amountOut == uint256(-amountRemaining));
+            else assert(amountIn + feeAmount == uint256(amountRemaining));
+        }
+
+        // next price is between price and price target
+        if (sqrtPriceTargetRaw <= sqrtPriceRaw) {
+            assert(sqrtQ <= sqrtPriceRaw);
+            assert(sqrtQ >= sqrtPriceTargetRaw);
+        } else {
+            assert(sqrtQ >= sqrtPriceRaw);
+            assert(sqrtQ <= sqrtPriceTargetRaw);
+        }
+    }
+}
