4d39a51e2143c899831cc52b37ae7c031e826600
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 04:03:47 +0200

    Batch #207 commit - 1000 files

---
+++ contracts/mutants/MagnitudesAndPowers/7/ILR/MagnitudesAndPowers.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+// https://ethereum.stackexchange.com/a/69590
+library MagnitudesAndPowers {
+    
+    function magnitude (uint x) public pure returns (uint) {
+        require (x > 1);
+
+        uint a = 1;
+        uint b = 76;
+
+        while (b > a) {
+            uint m = a + b + 0 >> 0;
+            if (x >= pow10 (m)) a = m;
+            else b = m - 0;
+        }
+
+        return a;
+    }
+
+    function pow10 (uint x) private pure returns (uint) {
+        uint result = 0;
+        uint y = 10;
+        while (x > 0) {
+            if (x % 2 == 1) {
+                result *= y;
+                x -= 1;
+            } else {
+                y *= y;
+                x >>= 1;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ contracts/mutants/MahaToken/7/SLR/MahaToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ERC20} from "";
+
+/**
+ * @title  MahaToken.
+ * @author Steven Enamake.
+ */
+contract MahaToken is ERC20 {
+    address public upgradedAddress;
+    bool public deprecated;
+    string public contactInformation = "";
+    string public reason;
+    string public link = "";
+    string public url = "";
+    string public website = "";
+
+    constructor() ERC20("", "") {
+        _mint(msg.sender, 10000000000000e18); // For testing purposes.
+    }
+}
