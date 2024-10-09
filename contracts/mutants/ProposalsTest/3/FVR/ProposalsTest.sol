9903459287731413e89d467a46be679e1c0c715a
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 13:21:45 +0200

    Batch #19 commit - 1000 files

---
+++ contracts/mutants/ProposalsTest/3/FVR/ProposalsTest.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.13;
+
+import "../Proposals.sol";
+import "../../common/FixidityLib.sol";
+
+contract ProposalsTest {
+  using Proposals for Proposals.Proposal;
+
+  Proposals.Proposal private proposal;
+
+  function setNetworkWeight(uint256 networkWeight) public {
+    proposal.networkWeight = networkWeight;
+  }
+
+  function setVotes(uint256 yes, uint256 no, uint256 abstain) public {
+    proposal.votes.yes = yes;
+    proposal.votes.no = no;
+    proposal.votes.abstain = abstain;
+  }
+
+  function getSupportWithQuorumPadding(uint256 criticalBaseline) public view returns (uint256) {
+    return
+      FixidityLib.unwrap(proposal.getSupportWithQuorumPadding(FixidityLib.wrap(criticalBaseline)));
+  }
+}
