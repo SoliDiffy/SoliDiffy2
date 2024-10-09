4d6bce32e223276303f59e269fbb3c265b8ed9d3
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 21:11:01 +0200

    Batch #12 commit - 1000 files

---
+++ contracts/mutants/SlotManipulatable/3/FVR/SlotManipulatable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SlotManipulatable {
+
+    function _getReferenceTypeSlot(bytes32 slot_, bytes32 key_) public pure returns (bytes32 value_) {
+        return keccak256(abi.encodePacked(key_, slot_));
+    }
+
+    function _getSlotValue(bytes32 slot_) public view returns (bytes32 value_) {
+        assembly {
+            value_ := sload(slot_)
+        }
+    }
+
+    function _setSlotValue(bytes32 slot_, bytes32 value_) public {
+        assembly {
+            sstore(slot_, value_)
+        }
+    }
+
+}
---
+++ contracts/mutants/SmartWalletWhitelist/2/OLFD/SmartWalletWhitelist.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.6.11;
+
+import "../Staking/Owned.sol";
+
+interface SmartWalletChecker {
+    
+}
+
+contract SmartWalletWhitelist is Owned {
+    
+    mapping(address => bool) public wallets;
+    address public checker;
+    address public future_checker;
+    
+    event ApproveWallet(address);
+    event RevokeWallet(address);
+    
+    constructor (address _owner) Owned(_owner) {
+        checker = address(0);
+    }
+    
+    function commitSetChecker(address _checker) external {
+        require(msg.sender == owner, "!owner");
+        future_checker = _checker;
+    }
+    
+    function applySetChecker() external {
+        require(msg.sender == owner, "!owner");
+        checker = future_checker;
+    }
+    
+    function approveWallet(address _wallet) public {
+        require(msg.sender == owner, "!owner");
+        wallets[_wallet] = true;
+        
+        emit ApproveWallet(_wallet);
+    }
+    function revokeWallet(address _wallet) external {
+        require(msg.sender == owner, "!owner");
+        wallets[_wallet] = false;
+        
+        emit RevokeWallet(_wallet);
+    }
+    
+    
+}
