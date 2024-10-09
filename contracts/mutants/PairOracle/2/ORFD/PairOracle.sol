43a1fe71897e21f11f964eb0e7e01f406992b4c9
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 10:03:25 +0200

    Batch #256 commit - 1000 files

---
+++ contracts/mutants/PairOracle/2/ORFD/PairOracle.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/proxy/Initializable.sol";
+
+import "../libraries/UQ112x112.sol";
+import "../libraries/FixedPoint.sol";
+import "../interfaces/IPairOracle.sol";
+import "../interfaces/ISwapPair.sol";
+import "./Operator.sol";
+
+contract PairOracle is Operator, IPairOracle, Initializable {
+    using FixedPoint for *;
+    using SafeMath for uint256;
+
+    uint256 public PERIOD = 600; // 10-minute TWAP (time-weighted average price)
+
+    ISwapPair public pair;
+    address public token0;
+    address public token1;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+    uint32 public blockTimestampLast;
+    FixedPoint.uq112x112 public price0Average;
+    FixedPoint.uq112x112 public price1Average;
+
+    function initialize(address pairAddress) external onlyOwner initializer {
+        ISwapPair _pair = ISwapPair(pairAddress);
+        pair = _pair;
+        token0 = _pair.token0();
+        token1 = _pair.token1();
+        price0CumulativeLast = _pair.price0CumulativeLast(); // Fetch the current accumulated price value (1 / 0)
+        price1CumulativeLast = _pair.price1CumulativeLast(); // Fetch the current accumulated price value (0 / 1)
+        uint112 reserve0;
+        uint112 reserve1;
+        (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
+        require(reserve0 != 0 && reserve1 != 0, "vSwapPairOracle: NO_RESERVES"); // Ensure that there's liquidity in the pair
+    }
+
+    function setPeriod(uint256 _period) external onlyOperator {
+        PERIOD = _period;
+    }
+
+    
+
+    // Note this will always return 0 before update has been called successfully for the first time.
+    
+
+    function currentBlockTimestamp() internal view returns (uint32) {
+        return uint32(block.timestamp % 2**32);
+    }
+
+    // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
+    function currentCumulativePrices(address _pair)
+    internal
+    view
+    returns (
+        uint256 price0Cumulative,
+        uint256 price1Cumulative,
+        uint32 blockTimestamp
+    )
+    {
+        blockTimestamp = currentBlockTimestamp();
+        price0Cumulative = ISwapPair(_pair).price0CumulativeLast();
+        price1Cumulative = ISwapPair(_pair).price1CumulativeLast();
+
+        // if time has elapsed since the last update on the pair, mock the accumulated price values
+        (uint112 reserve0, uint112 reserve1, uint32 _blockTimestampLast) = ISwapPair(pair).getReserves();
+        if (_blockTimestampLast != blockTimestamp) {
+            // subtraction overflow is desired
+            uint32 timeElapsed = blockTimestamp - _blockTimestampLast;
+            // addition overflow is desired
+            // counterfactual
+            price0Cumulative += uint256(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
+            // counterfactual
+            price1Cumulative += uint256(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
+        }
+    }
+}
