76087ff43ec0bf4a97fcdf9672791fce441ea521
SoliDiffy <limonyassistant@gmail.com>
2024-10-09 13:44:33 +0200

    Batch #22 commit - 1000 files

---
+++ contracts/mutants/QValidator/4/ORFD/QValidator.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+/*
+      ___       ___       ___       ___       ___
+     /\  \     /\__\     /\  \     /\  \     /\  \
+    /::\  \   /:/ _/_   /::\  \   _\:\  \    \:\  \
+    \:\:\__\ /:/_/\__\ /::\:\__\ /\/::\__\   /::\__\
+     \::/  / \:\/:/  / \:\::/  / \::/\/__/  /:/\/__/
+     /:/  /   \::/  /   \::/  /   \:\__\    \/__/
+     \/__/     \/__/     \/__/     \/__/
+
+*
+* MIT License
+* ===========
+*
+* Copyright (c) 2021 QubitFinance
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+import "../interfaces/IPriceCalculator.sol";
+import "../interfaces/IQValidator.sol";
+import "../interfaces/IQToken.sol";
+import "../interfaces/IQore.sol";
+import "../library/QConstant.sol";
+
+contract QValidator is IQValidator, OwnableUpgradeable {
+    using SafeMath for uint;
+
+    /* ========== CONSTANT VARIABLES ========== */
+
+    IPriceCalculator public constant oracle = IPriceCalculator(0x20E5E35ba29dC3B540a1aee781D0814D5c77Bce6);
+
+    /* ========== STATE VARIABLES ========== */
+
+    IQore public qore;
+
+    /* ========== INITIALIZER ========== */
+
+    function initialize() external initializer {
+        __Ownable_init();
+    }
+
+    /* ========== VIEWS ========== */
+
+    
+
+    
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function setQore(address _qore) external onlyOwner {
+        require(_qore != address(0), "QValidator: invalid qore address");
+        require(address(qore) == address(0), "QValidator: qore already set");
+        qore = IQore(_qore);
+    }
+
+    /* ========== ALLOWED FUNCTIONS ========== */
+
+    
+
+    
+
+    function liquidateAllowed(
+        address qToken,
+        address borrower,
+        uint liquidateAmount,
+        uint closeFactor
+    ) external override returns (bool) {
+        // The borrower must have shortfall in order to be liquidate
+        (, uint shortfall) = _getAccountLiquidityInternal(borrower, address(0), 0, 0);
+        require(shortfall != 0, "QValidator: Insufficient shortfall");
+
+        // The liquidator may not repay more than what is allowed by the closeFactor
+        uint borrowBalance = IQToken(payable(qToken)).accruedBorrowBalanceOf(borrower);
+        uint maxClose = closeFactor.mul(borrowBalance).div(1e18);
+        return liquidateAmount <= maxClose;
+    }
+
+    function qTokenAmountToSeize(
+        address qTokenBorrowed,
+        address qTokenCollateral,
+        uint amount
+    ) external override returns (uint seizeQAmount) {
+        uint priceBorrowed = oracle.getUnderlyingPrice(qTokenBorrowed);
+        uint priceCollateral = oracle.getUnderlyingPrice(qTokenCollateral);
+        require(priceBorrowed != 0 && priceCollateral != 0, "QValidator: price error");
+
+        uint exchangeRate = IQToken(payable(qTokenCollateral)).accruedExchangeRate();
+        require(exchangeRate != 0, "QValidator: exchangeRate of qTokenCollateral is zero");
+
+        // seizeQTokenAmount = amount * (liquidationIncentive * priceBorrowed) / (priceCollateral * exchangeRate)
+        return amount.mul(qore.liquidationIncentive()).mul(priceBorrowed).div(priceCollateral.mul(exchangeRate));
+    }
+
+    /* ========== PRIVATE FUNCTIONS ========== */
+
+    function _getAccountLiquidityInternal(
+        address account,
+        address qToken,
+        uint redeemAmount,
+        uint borrowAmount
+    ) private returns (uint liquidity, uint shortfall) {
+        uint accCollateralValueInUSD;
+        uint accBorrowValueInUSD;
+
+        address[] memory assets = qore.marketListOf(account);
+        uint[] memory prices = oracle.getUnderlyingPrices(assets);
+        for (uint i = 0; i < assets.length; i++) {
+            require(prices[i] != 0, "QValidator: price error");
+            QConstant.AccountSnapshot memory snapshot = IQToken(payable(assets[i])).accruedAccountSnapshot(account);
+
+            uint collateralValuePerShareInUSD = snapshot
+                .exchangeRate
+                .mul(prices[i])
+                .mul(qore.marketInfoOf(payable(assets[i])).collateralFactor)
+                .div(1e36);
+            accCollateralValueInUSD = accCollateralValueInUSD.add(
+                snapshot.qTokenBalance.mul(collateralValuePerShareInUSD).div(1e18)
+            );
+            accBorrowValueInUSD = accBorrowValueInUSD.add(snapshot.borrowBalance.mul(prices[i]).div(1e18));
+
+            if (assets[i] == qToken) {
+                accBorrowValueInUSD = accBorrowValueInUSD.add(redeemAmount.mul(collateralValuePerShareInUSD).div(1e18));
+                accBorrowValueInUSD = accBorrowValueInUSD.add(borrowAmount.mul(prices[i]).div(1e18));
+            }
+        }
+
+        liquidity = accCollateralValueInUSD > accBorrowValueInUSD
+            ? accCollateralValueInUSD.sub(accBorrowValueInUSD)
+            : 0;
+        shortfall = accCollateralValueInUSD > accBorrowValueInUSD
+            ? 0
+            : accBorrowValueInUSD.sub(accCollateralValueInUSD);
+    }
+}
---
+++ contracts/mutants/QubitLocker/9/ORFD/QubitLocker.sol
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+/*
+      ___       ___       ___       ___       ___
+     /\  \     /\__\     /\  \     /\  \     /\  \
+    /::\  \   /:/ _/_   /::\  \   _\:\  \    \:\  \
+    \:\:\__\ /:/_/\__\ /::\:\__\ /\/::\__\   /::\__\
+     \::/  / \:\/:/  / \:\::/  / \::/\/__/  /:/\/__/
+     /:/  /   \::/  /   \::/  /   \:\__\    \/__/
+     \/__/     \/__/     \/__/     \/__/
+
+*
+* MIT License
+* ===========
+*
+* Copyright (c) 2021 QubitFinance
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+import "@openzeppelin/contracts/math/Math.sol";
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+import "../interfaces/IQubitLocker.sol";
+import "../library/WhitelistUpgradeable.sol";
+import "../library/SafeToken.sol";
+
+contract QubitLocker is IQubitLocker, WhitelistUpgradeable, ReentrancyGuardUpgradeable {
+    using SafeMath for uint;
+    using SafeToken for address;
+
+    /* ========== CONSTANTS ============= */
+
+    address public constant QBT = 0x17B7163cf1Dbd286E262ddc68b553D899B93f526;
+
+    uint public constant LOCK_UNIT_BASE = 7 days;
+    uint public constant LOCK_UNIT_MAX = 2 * 365 days;
+
+    /* ========== STATE VARIABLES ========== */
+
+    mapping(address => uint) public balances;
+    mapping(address => uint) public expires;
+
+    uint public override totalBalance;
+
+    uint private _lastTotalScore;
+    uint private _lastSlope;
+    uint private _lastTimestamp;
+    mapping(uint => uint) private _slopeChanges;
+
+    /* ========== INITIALIZER ========== */
+
+    function initialize() external initializer {
+        __WhitelistUpgradeable_init();
+        __ReentrancyGuard_init();
+        _lastTimestamp = block.timestamp;
+    }
+
+    /* ========== VIEWS ========== */
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    /**
+     * @notice Calculate time-weighted balance of account
+     * @param account Account of which the balance will be calculated
+     */
+    
+
+    function truncateExpiry(uint time) public pure returns (uint) {
+        return time.div(LOCK_UNIT_BASE).mul(LOCK_UNIT_BASE);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    
+
+    
+
+    /**
+     * @notice Withdraw all tokens for `msg.sender`
+     * @dev Only possible if the lock has expired
+     */
+    
+
+    function depositBehalf(
+        address account,
+        uint amount,
+        uint expiry
+    ) external override onlyWhitelisted nonReentrant {
+        require(amount > 0, "QubitLocker: invalid amount");
+
+        expiry = balances[account] == 0 ? truncateExpiry(expiry) : expires[account];
+        require(block.timestamp < expiry && expiry <= block.timestamp + LOCK_UNIT_MAX, "QubitLocker: invalid expiry");
+
+        _slopeChanges[expiry] = _slopeChanges[expiry].add(amount.div(LOCK_UNIT_MAX));
+        _updateTotalScore(amount, expiry);
+
+        QBT.safeTransferFrom(msg.sender, address(this), amount);
+        totalBalance = totalBalance.add(amount);
+
+        balances[account] = balances[account].add(amount);
+        expires[account] = expiry;
+    }
+
+    function withdrawBehalf(address account) external override onlyWhitelisted nonReentrant {
+        require(balances[account] > 0 && block.timestamp >= expires[account], "QubitLocker: invalid state");
+        _updateTotalScore(0, 0);
+
+        uint amount = balances[account];
+        totalBalance = totalBalance.sub(amount);
+        delete balances[account];
+        delete expires[account];
+        QBT.safeTransfer(account, amount);
+    }
+
+    /* ========== PRIVATE FUNCTIONS ========== */
+
+    function _updateTotalScore(uint newAmount, uint nextExpiry) private {
+        (uint score, uint slope) = totalScore();
+
+        if (newAmount > 0) {
+            uint slopeChange = newAmount.div(LOCK_UNIT_MAX);
+            uint newAmountDeltaScore = nextExpiry.sub(block.timestamp).mul(slopeChange);
+
+            slope = slope.add(slopeChange);
+            score = score.add(newAmountDeltaScore);
+        }
+
+        _lastTotalScore = score;
+        _lastSlope = slope;
+        _lastTimestamp = block.timestamp;
+    }
+
+    function _updateTotalScoreExtendingLock(
+        uint amount,
+        uint prevExpiry,
+        uint nextExpiry
+    ) private {
+        (uint score, uint slope) = totalScore();
+
+        uint deltaScore = nextExpiry.sub(prevExpiry).mul(amount.div(LOCK_UNIT_MAX));
+        score = score.add(deltaScore);
+
+        _lastTotalScore = score;
+        _lastSlope = slope;
+        _lastTimestamp = block.timestamp;
+    }
+}
