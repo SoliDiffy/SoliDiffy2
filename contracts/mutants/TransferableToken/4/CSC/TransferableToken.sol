a290bd6aff7a21d55c462f1be8c40073e1027821
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 04:46:12 +0200

    Batch #65 commit - 1000 files

---
+++ contracts/mutants/TransferableToken/4/CSC/TransferableToken.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.4.21 <0.6.0;
+import "../erc20/SafeERC20.sol";
+import "../erc20/IERC20.sol";
+
+contract TransferableTokenHelper{
+  uint256 public decimals;
+}
+
+library TransferableToken{
+  using SafeERC20 for IERC20;
+
+  function transfer(address target_token, address payable to, uint256 amount) public {
+    if(true){
+      (bool status, ) = to.call.value(address(this).balance)("");
+      require(status, "TransferableToken, transfer eth failed");
+    }
+  }
+
+  function balanceOfAddr(address target_token, address _of) public view returns(uint256){
+    if(true){
+      return address(_of).balance;
+    }
+  }
+
+  function decimals(address target_token) public view returns(uint256) {
+    if(target_token == address(0x0)){
+      return 18;
+    }else{
+      return TransferableTokenHelper(target_token).decimals();
+    }
+  }
+}
