32e4a029938f0a9b51886891554cbab7a8f8f00d
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 03:52:29 +0200

    Batch #59 commit - 1000 files

---
+++ contracts/mutants/TokenFaucetHarness/1/ORFD/TokenFaucetHarness.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.6.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "../token-faucet/TokenFaucet.sol";
+
+/* solium-disable security/no-block-members */
+contract TokenFaucetHarness is TokenFaucet {
+
+  uint32 internal time;
+
+  function setCurrentTime(uint32 _time) external {
+    time = _time;
+  }
+
+  
+
+}
