b39936473ba8006069124ee079f6ae205d04fb02
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 09:51:03 +0200

    Batch #21 commit - 1000 files

---
+++ contracts/mutants/arbiUSDC/2/SLR/arbiUSDC.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.0;
+import "../ERC20.sol";
+
+// Mock arbiUSDC token
+contract arbiUSDC is ERC20("", "") {
+    // Nothing here
+}
