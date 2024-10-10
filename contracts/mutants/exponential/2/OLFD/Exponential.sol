c9096bb45317ce2fe3480a16e8a915fb3cdf2d7c
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 09:51:49 +0200

    Batch #25 commit - 1000 files

---
+++ contracts/mutants/exponential/2/OLFD/Exponential.sol
@@ -0,0 +1,161 @@
+pragma solidity ^0.5.16;
+
+import "./CarefulMath.sol";
+import "./ExponentialNoError.sol";
+
+/**
+ * @title Exponential module for storing fixed-precision decimals
+ * @author Rifi
+ * @dev Legacy contract for compatibility reasons with existing contracts that still use MathError
+ * @notice Exp is a struct which stores decimals with a fixed precision of 18 decimal places.
+ *         Thus, if we wanted to store the 5.1, mantissa would store 5.1e18. That is:
+ *         `Exp({mantissa: 5100000000000000000})`.
+ */
+contract Exponential is CarefulMath, ExponentialNoError {
+    /**
+     * @dev Creates an exponential from numerator and denominator values.
+     *      Note: Returns an error if (`num` * 10e18) > MAX_INT,
+     *            or if `denom` is zero.
+     */
+    function getExp(uint num, uint denom) pure internal returns (MathError, Exp memory) {
+        (MathError err0, uint scaledNumerator) = mulUInt(num, expScale);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+
+        (MathError err1, uint rational) = divUInt(scaledNumerator, denom);
+        if (err1 != MathError.NO_ERROR) {
+            return (err1, Exp({mantissa: 0}));
+        }
+
+        return (MathError.NO_ERROR, Exp({mantissa: rational}));
+    }
+
+    /**
+     * @dev Adds two exponentials, returning a new exponential.
+     */
+    function addExp(Exp memory a, Exp memory b) pure internal returns (MathError, Exp memory) {
+        (MathError error, uint result) = addUInt(a.mantissa, b.mantissa);
+
+        return (error, Exp({mantissa: result}));
+    }
+
+    /**
+     * @dev Subtracts two exponentials, returning a new exponential.
+     */
+    function subExp(Exp memory a, Exp memory b) pure internal returns (MathError, Exp memory) {
+        (MathError error, uint result) = subUInt(a.mantissa, b.mantissa);
+
+        return (error, Exp({mantissa: result}));
+    }
+
+    /**
+     * @dev Multiply an Exp by a scalar, returning a new Exp.
+     */
+    function mulScalar(Exp memory a, uint scalar) pure internal returns (MathError, Exp memory) {
+        (MathError err0, uint scaledMantissa) = mulUInt(a.mantissa, scalar);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+
+        return (MathError.NO_ERROR, Exp({mantissa: scaledMantissa}));
+    }
+
+    /**
+     * @dev Multiply an Exp by a scalar, then truncate to return an unsigned integer.
+     */
+    function mulScalarTruncate(Exp memory a, uint scalar) pure internal returns (MathError, uint) {
+        (MathError err, Exp memory product) = mulScalar(a, scalar);
+        if (err != MathError.NO_ERROR) {
+            return (err, 0);
+        }
+
+        return (MathError.NO_ERROR, truncate(product));
+    }
+
+    /**
+     * @dev Multiply an Exp by a scalar, truncate, then add an to an unsigned integer, returning an unsigned integer.
+     */
+    function mulScalarTruncateAddUInt(Exp memory a, uint scalar, uint addend) pure internal returns (MathError, uint) {
+        (MathError err, Exp memory product) = mulScalar(a, scalar);
+        if (err != MathError.NO_ERROR) {
+            return (err, 0);
+        }
+
+        return addUInt(truncate(product), addend);
+    }
+
+    /**
+     * @dev Divide an Exp by a scalar, returning a new Exp.
+     */
+    function divScalar(Exp memory a, uint scalar) pure internal returns (MathError, Exp memory) {
+        (MathError err0, uint descaledMantissa) = divUInt(a.mantissa, scalar);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+
+        return (MathError.NO_ERROR, Exp({mantissa: descaledMantissa}));
+    }
+
+    /**
+     * @dev Divide a scalar by an Exp, returning a new Exp.
+     */
+    function divScalarByExp(uint scalar, Exp memory divisor) pure internal returns (MathError, Exp memory) {
+        /*
+          We are doing this as:
+          getExp(mulUInt(expScale, scalar), divisor.mantissa)
+
+          How it works:
+          Exp = a / b;
+          Scalar = s;
+          `s / (a / b)` = `b * s / a` and since for an Exp `a = mantissa, b = expScale`
+        */
+        (MathError err0, uint numerator) = mulUInt(expScale, scalar);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+        return getExp(numerator, divisor.mantissa);
+    }
+
+    /**
+     * @dev Divide a scalar by an Exp, then truncate to return an unsigned integer.
+     */
+    function divScalarByExpTruncate(uint scalar, Exp memory divisor) pure internal returns (MathError, uint) {
+        (MathError err, Exp memory fraction) = divScalarByExp(scalar, divisor);
+        if (err != MathError.NO_ERROR) {
+            return (err, 0);
+        }
+
+        return (MathError.NO_ERROR, truncate(fraction));
+    }
+
+    /**
+     * @dev Multiplies two exponentials, returning a new exponential.
+     */
+    
+
+    /**
+     * @dev Multiplies two exponentials given their mantissas, returning a new exponential.
+     */
+    
+
+    /**
+     * @dev Multiplies three exponentials, returning a new exponential.
+     */
+    function mulExp3(Exp memory a, Exp memory b, Exp memory c) pure internal returns (MathError, Exp memory) {
+        (MathError err, Exp memory ab) = mulExp(a, b);
+        if (err != MathError.NO_ERROR) {
+            return (err, ab);
+        }
+        return mulExp(ab, c);
+    }
+
+    /**
+     * @dev Divides two exponentials, returning a new exponential.
+     *     (a/scale) / (b/scale) = (a/scale) * (scale/b) = a/b,
+     *  which we can scale as an Exp by calling getExp(a.mantissa, b.mantissa)
+     */
+    function divExp(Exp memory a, Exp memory b) pure internal returns (MathError, Exp memory) {
+        return getExp(a.mantissa, b.mantissa);
+    }
+}
