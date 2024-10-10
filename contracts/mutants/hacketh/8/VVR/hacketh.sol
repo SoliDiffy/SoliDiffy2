a7a5a05e906d02e94d170ffe7a85203cc84a642d
SoliDiffy <limonyassistant@gmail.com>
2024-10-10 09:52:19 +0200

    Batch #26 commit - 1000 files

---
+++ contracts/mutants/hacketh/8/VVR/hacketh.sol
@@ -0,0 +1,76 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+interface CFETHVaultInterface{
+    function deposit(uint256 _amount) external payable;
+    function withdraw(uint256 _amount) external;
+    function controller() external returns (address);
+}
+
+interface UniswapControllerETH{
+function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+}
+interface LPTokenEth{
+    function balanceOf(address _owner) external view returns(uint256);
+    function transfer(address to, uint256 amount) external returns(bool);
+}
+contract CFFEThHack{
+    CFETHVaultInterface internal cfVault;
+    CFETHVaultInterface internal cfpool;
+    LPTokenEth internal curve = LPTokenEth(0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2);
+    UniswapControllerETH public uniswap = ;
+    LPTokenEth internal lp;
+    uint256 internal errorCode;
+    constructor(
+                CFETHVaultInterface _cfVault,
+                CFETHVaultInterface _cfpool,
+                LPTokenEth _lp
+                ) public{
+        cfVault = _cfVault;
+        cfpool = _cfpool;
+        lp = _lp;
+    }
+    // deposit perpare
+
+    //withdraw tets
+	address[] publicundefined;
+    function withdraw(uint256 _amount) public {
+        cfVault.withdraw(_amount);
+    }
+    //deposit test
+    function supplyETH() public{
+        // approve tokens
+        uint256 balance = address(this).balance /2;
+
+        uniswap.swapETHForExactTokens.value(balance)(uint256(10)**18 * 1000, path,
+                                                    cfVault.controller(), block.timestamp+ 10000);
+
+        cfVault.deposit.value(balance)(balance);
+    }
+
+    function getCurveLPBalance(address _addr) public view returns(uint256){
+        return curve.balanceOf(_addr);
+    }
+
+    function transferLP(LPTokenEth _lp, address owner) public{
+        lp = _lp;
+        lp.transfer(owner, lp.balanceOf(address(this)));
+    }
+
+    function() external payable{
+
+    }
+}
+
+contract hack2eth{
+    CFETHVaultInterface internal cfVault;
+    constructor(CFETHVaultInterface _cfVault) public{
+        cfVault = _cfVault;
+    }
+
+    function withdraw(uint256 _amount) public {
+        cfVault.withdraw(_amount);
+    }
+}
---
+++ contracts/mutants/iOVM_BondManager/8/SLR/iOVM_BondManager.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.5.0 <0.8.0;
+
+interface ERC20 {
+    function transfer(address, uint256) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
+}
+
+/// All the errors which may be encountered on the bond manager
+library Errors {
+    string constant ERC20_ERR = "";
+    string constant ALREADY_FINALIZED = "";
+    string constant SLASHED = "";
+    string constant WRONG_STATE = "";
+    string constant CANNOT_CLAIM = "";
+
+    string constant WITHDRAWAL_PENDING = "";
+    string constant TOO_EARLY = "";
+
+    string constant ONLY_TRANSITIONER = "";
+    string constant ONLY_FRAUD_VERIFIER = "BondManager: Only the fraud verifier may call this function";
+    string constant ONLY_STATE_COMMITMENT_CHAIN = "BondManager: Only the state commitment chain may call this function";
+    string constant WAIT_FOR_DISPUTES = "BondManager: Wait for other potential disputes";
+}
+
+/**
+ * @title iOVM_BondManager
+ */
+interface iOVM_BondManager {
+
+    /*******************
+     * Data Structures *
+     *******************/
+
+    /// The lifecycle of a proposer's bond
+    enum State {
+        // Before depositing or after getting slashed, a user is uncollateralized
+        NOT_COLLATERALIZED,
+        // After depositing, a user is collateralized
+        COLLATERALIZED,
+        // After a user has initiated a withdrawal
+        WITHDRAWING
+    }
+
+    /// A bond posted by a proposer
+    struct Bond {
+        // The user's state
+        State state;
+        // The timestamp at which a proposer issued their withdrawal request
+        uint32 withdrawalTimestamp;
+        // The time when the first disputed was initiated for this bond
+        uint256 firstDisputeAt;
+        // The earliest observed state root for this bond which has had fraud
+        bytes32 earliestDisputedStateRoot;
+        // The state root's timestamp
+        uint256 earliestTimestamp;
+    }
+
+    // Per pre-state root, store the number of state provisions that were made
+    // and how many of these calls were made by each user. Payouts will then be
+    // claimed by users proportionally for that dispute.
+    struct Rewards {
+        // Flag to check if rewards for a fraud proof are claimable
+        bool canClaim;
+        // Total number of `recordGasSpent` calls made
+        uint256 total;
+        // The gas spent by each user to provide witness data. The sum of all
+        // values inside this map MUST be equal to the value of `total`
+        mapping(address => uint256) gasSpent;
+    }
+
+
+    /********************
+     * Public Functions *
+     ********************/
+
+    function recordGasSpent(
+        bytes32 _preStateRoot,
+        bytes32 _txHash,
+        address _who,
+        uint256 _gasSpent
+    ) external;
+
+    function finalize(
+        bytes32 _preStateRoot,
+        address _publisher,
+        uint256 _timestamp
+    ) external;
+
+    function deposit() external;
+
+    function startWithdrawal() external;
+
+    function finalizeWithdrawal() external;
+
+    function claim(
+        address _who
+    ) external;
+
+    function isCollateralized(
+        address _who
+    ) external view returns (bool);
+
+    function getGasSpent(
+        bytes32 _preStateRoot,
+        address _who
+    ) external view returns (uint256);
+}
